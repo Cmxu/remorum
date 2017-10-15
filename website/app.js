ed652e392d227baabaaef28429c6b573b041623a
ashley <msashleychen@gmail.com>
2017-10-15 04:58:34 -0400

    JSON current location setup

--- website/app.js
+++ website/app.js
@@ -5,6 +5,10 @@ var app = express();
 const url = require('url');    
 var mongoose = require("mongoose");
 var MongoClient = require('mongodb').MongoClient;
+var NodeGeocoder = require('node-geocoder');
+var options = {
+  provider: 'google',
+};
 
 mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/node-demo");
 app.use(bodyParser()); 
@@ -28,13 +32,40 @@ app.post('/feeder', function(req, res){
     console.log('POST /');
     console.dir(req.body);
     var myData = new User(req.body);
+    var parsedData = filterLocation(req.body);
+    console.log(parsedData);
+    //makeJSON(parsedData);
  	myData.save()
  		.then(item => {
  		console.log("Submitted data to Mongo success");
- 		res.status(200).redirect("submitted.html");
+ 		res.status(200).redirect("map.html");
  	});
     
 });
+
+function filterLocation(data){
+    var location = data['feeder_location'];
+    var geocoder = NodeGeocoder(options);
+    geocoder.geocode(location)
+      .then(function(res) {
+        data['lat'] = res[0]['latitude'];
+        data['lng'] = res[0]['longitude'];
+        var obj = {
+       table: []
+    };
+    obj.table.push(data);
+    var json = JSON.stringify(obj);
+    fs.writeFile ("input.json", JSON.stringify(obj), function(err) {
+        if (err) throw err;
+        console.log('complete');
+        }
+    );
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+
+}
 
 app.get('/hungry', function(req, res){
     console.log('GET /')
