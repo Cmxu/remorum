952ef6869502f3843ecb7357027d77798be91cb6
Unknown <c.xu@wustl.edu>
2017-10-15 07:57:07 -0400

    web

--- website/js/script.js
+++ website/js/script.js
@@ -28,7 +28,33 @@ function initMap() {
   var uluru = {lat: 40.7128, lng: -74.0060};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
-    center: uluru
+    center: uluru,
+    disableDefaultUI: true,
+    disableDoubleClickZoom: true,
+    scrollwheel: false,
+    styles: [{"elementType": "geometry","stylers": [{"color": "#98dbc6"}]},
+            {"elementType": "labels","stylers": [{"visibility": "off"}]},
+            {"elementType": "labels.icon","stylers": [{"visibility": "off"}]},
+            {"elementType": "labels.text.fill","stylers": [{"color": "#616161"}]},
+            {"elementType": "labels.text.stroke","stylers": [{"color": "#f5f5f5"}]},
+            {"featureType": "administrative.land_parcel","elementType": "labels.text.fill","stylers": [{"color": "#bdbdbd"}]},
+            {"featureType": "administrative.neighborhood","stylers": [{"visibility": "off"}]},
+            {"featureType": "poi","elementType": "geometry","stylers": [{"color": "#5bc8ac"}]},
+            {"featureType": "poi","elementType": "labels.text.fill","stylers": [{"color": "#757575"}]},
+            {"featureType": "poi.business","stylers": [{"visibility": "off"}]},
+            {"featureType": "poi.park","elementType": "labels.text","stylers": [{"visibility": "off"}]},
+            {"featureType": "poi.park","elementType": "labels.text.fill","stylers": [{"color": "#9e9e9e"}]},
+            {"featureType": "road","stylers": [{"visibility": "off"}]},
+            {"featureType": "road","elementType": "geometry","stylers": [{"color": "#34675c"}]},
+            {"featureType": "road.arterial","elementType": "labels.text.fill","stylers": [{"color": "#757575"}]},
+            {"featureType": "road.highway","elementType": "geometry","stylers": [{"color": "#dadada"}]},
+            {"featureType": "road.highway","elementType": "labels.text.fill","stylers": [{"color": "#616161"}]},
+            {"featureType": "road.local","elementType": "labels.text.fill","stylers": [{"color": "#9e9e9e"}]},
+            {"featureType": "transit","elementType": "geometry","stylers": [{"color": "#ffffff"}]},
+            {"featureType": "transit.station","elementType": "geometry","stylers": [{"color": "#34675c"},{"visibility": "off"}]},
+            {"featureType": "water","elementType": "geometry","stylers": [{"color": "#c9c9c9"}]},
+            {"featureType": "water","elementType": "geometry.fill","stylers": [{"color": "#4cb5f5"}]},
+            {"featureType": "water","elementType": "labels.text.fill","stylers": [{"color": "#9e9e9e"}]}]
   });
   var image = 'http://aaltofimedia1.aalto.fi/midcom-static/org.routamc.positioning/pin-regular.png';
   var marker = new google.maps.Marker({
